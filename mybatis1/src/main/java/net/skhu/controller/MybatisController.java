10e09818645ef89cd8c33dc2b509670c47d311d3
nsap19 <zzz0484@naver.com>
2020-03-20 18:40:36 +0900

    Mar20 - mybatis dynamicSQL

--- mybatis1/src/main/java/net/skhu/controller/MybatisController.java
+++ mybatis1/src/main/java/net/skhu/controller/MybatisController.java
@@ -10,14 +10,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import net.skhu.dto.Department;
 import net.skhu.dto.Student;
+import net.skhu.dto.User;
 import net.skhu.mapper.DepartmentMapper;
 import net.skhu.mapper.StudentMapper;
+import net.skhu.mapper.UserMapper;
 
 @Controller
 @RequestMapping("mybatis")
 public class MybatisController {
 	@Autowired DepartmentMapper departmentMapper;
 	@Autowired StudentMapper studentMapper;
+	@Autowired UserMapper userMapper;
 
 	@RequestMapping(value = "cacheTest", method = RequestMethod.GET)
 	public String cacheTest(Model model) {
@@ -48,5 +51,43 @@ public class MybatisController {
 	public String departmentList2(Model model) {
 		model.addAttribute("departments",departmentMapper.findAllWithStudents());
 		return "mybatis/departmentList";
+	}
+
+	@RequestMapping("dynamicSQL")
+	public String dynamicSQL(Model model) {
+		//전체 학생 목록 출력 : list1
+		List<User> list1 = userMapper.findAllOrderBy(5,"name DESC");
+		model.addAttribute("list1", list1);
+
+		//id=2, name="이승진"
+
+		//"이승진"이라는 이름으로 유저 검색 : list2
+		User user1 = new User();
+		user1.setName("이승진");
+		List<User> list2 = userMapper.findByNameOrUserid(user1);
+		model.addAttribute("list2", list2);
+
+		//id=2인 사람의 name을 "Lee, Seungjin"으로 수정
+		User user3 = new User();
+		user3.setId(2);
+		user3.setName("Lee, Seungjin");
+		userMapper.update(user3);
+
+		//"lsj"라는 아이디로 유저 검색 : list3
+		User user2 = new User();
+		user2.setUserid("lsj");
+		List<User> list3 = userMapper.findByNameOrUserid2(user2);
+		model.addAttribute("list3", list3); //lsj라는 userid를 가진 사람의 name은 Lee, Seungjin으로 출력
+
+		//user3의 이름을 "Lee, Seungjin"에서 "이승진"으로 수정
+		user3.setName("이승진");
+		userMapper.update(user3);
+
+		// 2,3,5의 id를 가진 학생 검색 = list4
+		int[] a = new int[] {2,3,5};
+		List<User> list4 = userMapper.findByIdList(a);
+		model.addAttribute("list4", list4); //id=2인 user의 이름을 Lee,Seungjin에서 이승진으로 수정한 정보가 출력
+
+		return "mybatis/dynamicSQL";
 	}
 }
